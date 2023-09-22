9b00b685edb8fcea1b563c3a51d033d00493b295
Yijun <github@yps.staut.land>
2023-09-22 13:35:51 +0200

    Add files for Hans

--- detect_chessboard.py
+++ detect_chessboard.py
@@ -1,43 +1,84 @@
 import cv2
 import numpy as np
-import logging
 
-# Setting up logging
-logging.basicConfig(level=logging.INFO)
+def auto_canny(image, sigma=0.33):
+    v = np.median(image)
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+    return edged
 
-# This script does the following:
-#   Reads an image file named 'chessboard.jpg'. Replace 'chessboard.jpg' with the name of your image file.
-#   Converts the image to grayscale.
-#   Tries to find the corners of a chessboard in the image. (8,8) specifies that it's an 8x8 chessboard.
-#   Change these values if you have a different sized chessboard.
-#   If the chessboard is found (ret is True), it draws the corners on the image.
-#   It displays the image and waits until you close the image window.
-# Please note that this script is very basic and might not work perfectly with all images. 
-# The chessboard needs to be clearly visible and the image needs to be of a certain quality. 
-# The image should also be taken from a straight angle. This script doesn't handle piece detection, 
-# which is a more complex task. It's a first step to get you started on your project.
+img = cv2.imread("chessboard4.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Load image
-image = cv2.imread('chessboard.jpg')
+cv2.imshow("Grayscale Image", gray)
+cv2.waitKey(0)
 
-if image is None:
-    logging.error('Image not found. Please check the filename and try again.')
-else:
-    logging.info('Image loaded successfully.')
-    # Convert to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("grayscale.jpg", gray)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (8,8), None)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edges = auto_canny(blurred)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        logging.info('Chessboard found. Drawing the corners...')
-        # Draw and display the corners
-        cv2.drawChessboardCorners(image, (8,8), corners, ret)
-        
-        cv2.imshow('Image', image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+
+board_contour = None
+for contour in contours:
+    perimeter = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
+    
+    if len(approx) > 4:
+        rect = cv2.minAreaRect(approx)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+
+        board_contour = np.array([box], dtype = "float32")
+
     else:
-        logging.info('No chessboard could be found in the image.')
+        board_contour = approx
+
+    if board_contour is not None and len(board_contour) > 0:
+        break
+
+if board_contour is None:
+    print("Chessboard not found in image.")
+else:
+    if len(board_contour) > 0:
+        print(board_contour)
+
+        board_contour = board_contour.reshape(-1, 1, 2).astype(np.int32)  # Reshape and convert to integer
+
+        # Draw contour
+        cv2.drawContours(img, [board_contour], -1, (0, 255, 0), 2)
+
+        # Warp perspective to get top-down view of chessboard
+        pts1 = np.float32(board_contour)
+        # pts2 = np.float32([[0, 0], [0, 480], [480, 480], [480, 0]])  # assuming a square chessboard of 480x480 pixels
+        pts2 = np.float32([[0, 0], [480, 0], [480, 480], [0, 480]])  # ordered: top-left, top-right, bottom-right, bottom-left
+
+        s = np.sum(pts1, axis=2)
+        diff = np.diff(pts1, axis=2)
+        
+        top_left = pts1[np.argmin(s)]
+        top_right = pts1[np.argmax(diff)]
+        bottom_right = pts1[np.argmax(s)]
+        bottom_left = pts1[np.argmin(diff)]
+        
+        pts1_ordered = np.array([top_left, top_right, bottom_right, bottom_left], dtype = "float32")
+
+        print("pts1 :");
+        print(pts1);
+        print("pts2 :");
+        print(pts2);
+        # matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        matrix = cv2.getPerspectiveTransform(pts1_ordered, pts2)
+        result = cv2.warpPerspective(img, matrix, (480, 480))
+
+        cv2.circle(img, tuple(map(int, top_left[0])), 10, (255, 0, 0), -1) # Blue
+        cv2.circle(img, tuple(map(int, top_right[0])), 10, (0, 255, 0), -1) # Green
+        cv2.circle(img, tuple(map(int, bottom_right[0])), 10, (0, 0, 255), -1) # Red
+        cv2.circle(img, tuple(map(int, bottom_left[0])), 10, (255, 255, 0), -1) # Cyan
+
+
+        cv2.imshow("Chessboard", result)
+        cv2.waitKey(0)
